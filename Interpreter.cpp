72935a4a96f68e2fb547866e39cfefd2f70b905e
feliposz <feliposz@gmail.com>
2024-01-25 04:08:41 -0300

    Chapter 7: An Interpreter (implemented print, repeat, if statements)

--- Interpreter.cpp
+++ Interpreter.cpp
@@ -58,6 +58,71 @@ int eval_seq(json &expr, map<string, int> &env)
     return result;
 }
 
+int eval_print(json &expr, map<string, int> &env)
+{
+    assert(expr.size() > 1);
+    for (int i = 1; i < expr.size(); i++)
+    {
+        if (expr[i].is_string())
+        {
+            cout << expr[i].get<string>();
+        }
+        else
+        {
+            cout << eval(expr[i], env);
+        }
+        cout << " ";
+    }
+    cout << endl;
+    return 0;
+}
+
+int eval_repeat(json &expr, map<string, int> &env)
+{
+    assert(expr.size() == 3);
+    assert(expr[1].is_number_integer());
+    int countdown = expr[1].get<int>();
+    int result = 0;
+    while (countdown-- > 0)
+    {
+        result = eval(expr[2], env);
+    }
+    return 0;
+}
+
+int eval_if(json &expr, map<string, int> &env)
+{
+    assert(expr.size() == 4);
+    if (eval(expr[1], env))
+    {
+        return eval(expr[2], env);
+    }
+    else
+    {
+        return eval(expr[3], env);
+    }
+}
+
+int eval_leq(json &expr, map<string, int> &env)
+{
+    assert(expr.size() == 3);
+    int left = eval(expr[1], env);
+    int right = eval(expr[2], env);
+    return left <= right;
+}
+
+map<string, int(*)(json&, map<string, int>&)> ops = {
+    { "abs", eval_abs },
+    { "add", eval_add },
+    { "seq", eval_seq },
+    { "set", eval_set },
+    { "get", eval_get },
+    { "print", eval_print },
+    { "repeat", eval_repeat },
+    { "if", eval_if},
+    { "leq", eval_leq},
+};
+
 int eval(json &expr, map<string, int> &env)
 {
     if (expr.is_number_integer())
@@ -69,47 +134,64 @@ int eval(json &expr, map<string, int> &env)
     assert(expr.size() > 0);
 
     string op = expr[0].get<string>();
-    if (op == "abs")
-    {
-        return eval_abs(expr, env);
-    }
-    else if (op == "add")
-    {
-        return eval_add(expr, env);
-    }
-    else if (op == "seq")
-    {
-        return eval_seq(expr, env);
-    }
-    else if (op == "set")
-    {
-        return eval_set(expr, env);
-    }
-    else if (op == "get")
-    {
-        return eval_get(expr, env);
-    }
 
-    assert(!"Unknown operation");
+    if (ops.count(op))
+    {
+        int result = ops[op](expr, env);
+        //cout << "\t[DEBUG]\top = " << op << "\tresult = " << result << endl;
+        return result;
+    }
+    
+    assert(!"Unknown operation");    
 }
 
 void interpreter_main()
 {
     cout << "Interpreter" << endl;
 
-    map<string, int> env1;
-    auto program1 = json::parse(R"(["add", ["abs", -3], 2])");
-    cout << "=> " << eval(program1, env1) << endl;
+    {
+        map<string, int> env;
+        auto program = json::parse(R"(["add", ["abs", -3], 2])");
+        int result = eval(program, env);
+        cout << "=> " << result << endl;
+    }
 
-    map<string, int> env2;
-    auto program2 = json::parse(R"(
-        [
-            "seq",
-            ["set", "alpha", 1],
-            ["set", "beta", 2],
-            ["add", ["get", "alpha"], ["get", "beta"]]
-        ]
-    )");
-    cout << "=> " << eval(program2, env2) << endl;
+    {
+        map<string, int> env;
+        auto program = json::parse(R"(
+            [
+                "seq",
+                ["set", "alpha", 1],
+                ["set", "beta", 2],
+                ["add", ["get", "alpha"], ["get", "beta"]]
+            ]
+        )");
+        int result = eval(program, env);
+        cout << "=> " << result << endl;
+    }
 
+    {
+        map<string, int> env;
+        auto program = json::parse(R"(
+            [
+                "seq",
+                ["set", "a", 1],
+                ["print", "initial", ["get", "a"]],
+                [
+                    "repeat", 4,
+                    [
+                        "seq",
+                        ["set", "a", ["add", ["get", "a"], ["get", "a"]]],
+                        ["if",
+                            ["leq", ["get", "a"], 10],
+                            ["print", "small", ["get", "a"]],
+                            ["print", "large", ["get", "a"]]
+                        ]
+                    ]
+                ]
+            ]
+        )");
+        int result = eval(program, env);
+        cout << "=> " << result << endl;
+    }
 }
