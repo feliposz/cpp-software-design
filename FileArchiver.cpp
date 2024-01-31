5a45c15659f85762dc1eea569ddb56cae5855585
feliposz <feliposz@gmail.com>
2024-01-31 02:31:05 -0300

    Chapter 10: A File Archiver (implemented backup)

--- FileArchiver.cpp
+++ FileArchiver.cpp
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <time.h>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -34,24 +35,66 @@ void hash_all(vector<file_hash> &result, const string &root)
     }
 }
 
-filesystem::path test_path, saved_path;
+void write_manifest(const string &target, char *timestamp_str, vector<file_hash> &manifest, string &manifest_filename)
+{
+    if (!filesystem::exists(target))
+    {
+        filesystem::create_directory(target);
+    }
+    manifest_filename = target + '\\' + timestamp_str + ".csv";
+    ofstream out(manifest_filename);
+    out << "filename,hash" << endl;
+    for (const auto &fh : manifest)
+    {
+        out << fh.filename << "," << fh.hash << endl;
+    }
+    out.close();
+}
+
+void copy_files(const string &source, const string &target, vector<file_hash> &manifest)
+{
+    for (const auto &fh : manifest)
+    {
+        string source_path = source + '\\' + fh.filename;
+        string target_path = target + '\\' + fh.hash + ".bck";
+        if (!filesystem::exists(target_path))
+        {
+            filesystem::copy_file(source_path, target_path);
+        }
+    }
+}
+
+void backup(const string &source, const string &target, vector<file_hash> &manifest, string &manifest_filename)
+{
+    hash_all(manifest, source);
+    time_t timestamp = time(0);
+    char timestamp_str[80];
+    struct tm timeinfo;
+    localtime_s(&timeinfo, &timestamp);
+    strftime(timestamp_str, 80, "%Y%m%d_%H%M%S", &timeinfo);
+    write_manifest(target, timestamp_str, manifest, manifest_filename);
+    copy_files(source, target, manifest);
+}
+
+filesystem::path files_path, saved_path, backup_path;
 
 void test_init()
 {
     saved_path = filesystem::current_path();
-    test_path = filesystem::temp_directory_path().append("FileArchiverTest");
+    files_path = filesystem::temp_directory_path().append("FileArchiverTest");
+    backup_path = filesystem::temp_directory_path().append("FileArchiverBackup");
 }
 
 void test_setup()
 {
-    filesystem::create_directory(test_path);
-    filesystem::current_path(test_path);
+    filesystem::create_directory(files_path);
+    filesystem::current_path(files_path);
     filesystem::create_directory("sub_dir");
 
     ofstream a("a.txt"), b("b.txt"), c("sub_dir\\c.txt");
-    a.write("aaa", 3);
-    b.write("bbb", 3);
-    c.write("ccc", 3);
+    a << "aaa";
+    b << "bbb";
+    c << "ccc";
     a.close();
     b.close();
     c.close();
@@ -60,7 +103,8 @@ void test_setup()
 void test_teardown()
 {
     filesystem::current_path(saved_path);
-    filesystem::remove_all(test_path);
+    filesystem::remove_all(files_path);
+    filesystem::remove_all(backup_path);
 }
 
 void test_nested_example()
@@ -85,7 +129,7 @@ void test_hashing()
 {
     test_setup();
     vector<file_hash> result;
-    hash_all(result, test_path.string());
+    hash_all(result, files_path.string());
     set<string> expected = { "a.txt", "b.txt", "sub_dir\\c.txt" };
     for (const auto &fh : result)
     {
@@ -99,7 +143,7 @@ void test_change()
 {
     test_setup();
     vector<file_hash> original;
-    hash_all(original, test_path.string());
+    hash_all(original, files_path.string());
 
     map<string, string> original_map;
     for (const auto &fh : original)
@@ -108,11 +152,11 @@ void test_change()
     }
 
     ofstream a("a.txt");
-    a.write("XXX", 3);
+    a << "XXX";
     a.close();
 
     vector<file_hash> changed;
-    hash_all(changed, test_path.string());
+    hash_all(changed, files_path.string());
 
     for (const auto &fh : changed)
     {
@@ -128,6 +172,21 @@ void test_change()
     test_teardown();
 }
 
+void test_backup()
+{
+    test_setup();
+    vector<file_hash> manifest;
+    string manifest_filename;
+    backup(files_path.string(), backup_path.string(), manifest, manifest_filename);
+    for (const auto &fh : manifest)
+    {
+        string file_path = backup_path.string() + '\\' + fh.hash + ".bck";
+        assert(filesystem::exists(file_path));
+    }
+    assert(filesystem::exists(manifest_filename));
+    test_teardown();
+}
+
 void archiver_main()
 {
     cout << "File Archiver:" << endl;
@@ -137,7 +196,7 @@ void archiver_main()
     test_deletion_example();
     test_hashing();
     test_change();
+    test_backup();
 
     cout << "All tests passed" << endl;
-
 }
